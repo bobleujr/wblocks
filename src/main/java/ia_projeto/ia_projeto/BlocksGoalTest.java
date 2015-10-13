beed1504ad92517e498adec5c7933e6952042649
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-13 15:58:04 -0300

    Adapted Jugs' GoalTest to Blocks

--- src/main/java/ia_projeto/ia_projeto/BlocksGoalTest.java
+++ src/main/java/ia_projeto/ia_projeto/BlocksGoalTest.java
@@ -1,21 +1,62 @@
 package ia_projeto.ia_projeto;
 
+import java.util.ArrayList;
+
 import aima.core.search.framework.GoalTest;
 
-public class BlocksGoalTest implements GoalTest{
+/**
+ * Class has goal world state
+ * @author Andre
+ * @author Daniel
+ * @author Paulo
+ */
+public class BlocksGoalTest extends State implements GoalTest{
+	 
 
-		
-	JawsState goal = new JawsState(2, 3);
-	    
-	@Override
-	public boolean isGoalState(Object state) {
-	        
-		JawsState jarra = (JawsState) state;
-	        
-		return jarra.equals(goal);
-	       
+	/**
+	 * Constructor for class GoalTest
+	 * @param world Matrix of chars to show how the world is
+	 * @param numberBlocks Store number of blocks in the world
+	 * @param sizeTable Store size of table in number of positions
+	 */
+	BlocksGoalTest(char[][] world, int numberBlocks, int sizeTable, ArrayList<String> function) {
+		super(world, numberBlocks, sizeTable, function); 
+	} 
+
+	/**
+	 * Return true if the current world is equal to goal world
+	 */
+	public boolean isGoalState(Object currentState) {
+		if(state().equals((World) currentState)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
+
+	/**
+	 * Prints the world
+	 */
+    protected void printState() {
+    	for(int i = state.world().length - 1; i >= 0; i--) {
+    		for(int j = 0; j < state.world()[i].length; j++) {
+    			System.out.print(state.world()[i][j]);
+    		}
+    		System.out.println();
+    	}
+    	for(int i = 0; i < state.world()[0].length; i++) {
+    		System.out.print('=');
+    	}
+    	System.out.println();
+    	
+    	for (String s : function) {
+    		System.out.println(s);
+    	}
+    }
+
+
 	
-	
+
 }
