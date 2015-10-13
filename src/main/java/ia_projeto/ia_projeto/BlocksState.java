a1f9bb554d2d14f747627a1edc3f13b261b02be9
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-13 15:58:35 -0300

    Updated possible actions to be done in the block's world

--- src/main/java/ia_projeto/ia_projeto/BlocksState.java
+++ src/main/java/ia_projeto/ia_projeto/BlocksState.java
@@ -1,27 +1,79 @@
 package ia_projeto.ia_projeto;
 
-public class BlocksState {
-	private Block a, b, c;
-	
-	
-	public Block aOn(){
-		return a.getOn();
-	}
-	
-	public Block bOn(){
-		return b.getOn();
-	}
+import java.util.ArrayList;
 
-	public Block cOn(){
-		return c.getOn();
-	}
+import aima.core.agent.Action;
+import aima.core.agent.impl.DynamicAction;
+
+/**
+ * Class has current world state and list of actions that can be done
+ * @author Andre
+ * @author Daniel
+ * @author Paulo
+ */
+public class BlocksState extends State{
 	
-	public void showBlocks(){
-		System.out.println("[A] is on ["+a.getOn().getId()+"]");
-		System.out.println("[B] is on ["+b.getOn().getId()+"]");
-		System.out.println("[C] is on ["+c.getOn().getId()+"]");
-	}
+	public Action MOVEA0 = new DynamicAction("MOVEA0");
+	public Action MOVEA1 = new DynamicAction("MOVEA1");
+	public Action MOVEA2 = new DynamicAction("MOVEA2");
+	public Action MOVEA3 = new DynamicAction("MOVEA3");
+	public Action MOVEB0 = new DynamicAction("MOVEB0");
+	public Action MOVEB1 = new DynamicAction("MOVEB1");
+	public Action MOVEB2 = new DynamicAction("MOVEB2");
+	public Action MOVEB3 = new DynamicAction("MOVEB3");
+	public Action MOVEC0 = new DynamicAction("MOVEC0");
+	public Action MOVEC1 = new DynamicAction("MOVEC1");
+	public Action MOVEC2 = new DynamicAction("MOVEC2");
+	public Action MOVEC3 = new DynamicAction("MOVEC3");
+	public Action MOVED0 = new DynamicAction("MOVED0");
+	public Action MOVED1 = new DynamicAction("MOVED1");
+	public Action MOVED2 = new DynamicAction("MOVED2");
+	public Action MOVED3 = new DynamicAction("MOVED3");
+	public Action MOVEAB = new DynamicAction("MOVEAB");
+	public Action MOVEAC = new DynamicAction("MOVEAC");
+	public Action MOVEAD = new DynamicAction("MOVEAD");
+	public Action MOVEBA = new DynamicAction("MOVEBA");
+	public Action MOVEBC = new DynamicAction("MOVEBC");
+	public Action MOVEBD = new DynamicAction("MOVEBD");
+	public Action MOVECA = new DynamicAction("MOVECA");
+	public Action MOVECB = new DynamicAction("MOVECB");
+	public Action MOVECD = new DynamicAction("MOVECD");
+	public Action MOVEDA = new DynamicAction("MOVEDA");
+	public Action MOVEDB = new DynamicAction("MOVEDB");
+	public Action MOVEDC = new DynamicAction("MOVEDC");
 	
+	/**
+	 * Constructor for class BlockState
+	 * @param world Matrix of chars to show how the world is
+	 * @param numberBlocks Store number of blocks in the world
+	 * @param sizeTable Store size of table in number of positions
+	 */
+    BlocksState(char[][] world, int numberBlocks, int sizeTable, ArrayList<String> function) {
+		super(world, numberBlocks, sizeTable, function);
+	} 	   
+
+    /**
+     * Print the world
+     */
+    protected void printState() {
+    	for(int i = state.world().length - 1; i >= 0; i--) {
+    		for(int j = 0; j < state.world()[i].length; j++) {
+    			System.out.print(state.world()[i][j]);
+    		}
+    		System.out.println();
+    	}
+    	for(int i = 0; i < state.world()[0].length; i++) {
+    		System.out.print('=');
+    	}
+    	System.out.println();
+    	
+    	for (String s : function) {
+    		System.out.println(s);
+    	}
+    	
+    }
+
+
 }
 
 
