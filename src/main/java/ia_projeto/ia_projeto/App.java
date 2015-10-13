56acf3b0f5e7a337a6b7d151acaa075d4cfdbf2b
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-13 15:57:41 -0300

    Implemented creation of initial and goal states

--- src/main/java/ia_projeto/ia_projeto/App.java
+++ src/main/java/ia_projeto/ia_projeto/App.java
@@ -1,13 +1,54 @@
 package ia_projeto.ia_projeto;
 
+import java.util.ArrayList;
 /**
- * Hello world!
- *
+ * Main Class that will start execution
+ * @author Andre
+ * @author Daniel
+ * @author Paulo
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+	/**
+	 * This class will create initial and goals char matrices
+	 * as well as lists of functions that describes the states.
+	 * With these informations, the class will create the initial
+	 * and goal states.
+	 */
+    public static void main(String[] args)    {
+    	char[][] mapState = {{' ','A',' ','B'},
+							 {' ','C',' ','D'}};
+
+    	char[][] mapGoal = {{' ','A',' ','B'},
+    					 	{' ','D',' ','C'}};
+    	
+    	ArrayList<String> stateFunction, goalFunction;
+    	stateFunction = new ArrayList<String>();
+    	goalFunction = new ArrayList<String>();
+    	
+    	stateFunction.add("clear(0)");
+    	stateFunction.add("on(A,table)");
+    	stateFunction.add("on(C,A)");
+    	stateFunction.add("clear(C)");
+    	stateFunction.add("clear(2)");
+    	stateFunction.add("on(B,table)");
+    	stateFunction.add("on(D,B)");
+    	stateFunction.add("clear(D)");
+    	
+    	goalFunction.add("clear(0)");
+    	goalFunction.add("on(A,table)");
+    	goalFunction.add("on(D,A)");
+    	stateFunction.add("clear(D)");
+    	goalFunction.add("clear(2)");
+    	goalFunction.add("on(B,table)");
+    	goalFunction.add("on(C,B)");
+    	stateFunction.add("clear(C)");
+    	
+    	BlocksState state = new BlocksState(mapState, 4, 4, stateFunction);
+        BlocksGoalTest goal = new BlocksGoalTest(mapGoal, 4, 4, goalFunction);
+        
+        state.printState();
+        goal.printState();
+        
     }
 }
