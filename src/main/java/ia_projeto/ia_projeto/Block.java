d7e1cf0218f05c0ca8ffa951b5b0cbbcfcd1293f
Andre Garcia <andrecadgarcia@gmail.com>
2015-10-19 00:44:17 -0200

    Added javadoc

--- src/main/java/ia_projeto/ia_projeto/Block.java
+++ src/main/java/ia_projeto/ia_projeto/Block.java
@@ -1,28 +1,55 @@
 package ia_projeto.ia_projeto;
 
+/**
+ * Block class 
+ * @author Andre
+ * @author Daniel
+ * @author Paulo
+ */
 public class Block {
 
-	char id;
-	String on;
-	int pos;
+	char id;	//ID for a block
+	String on;	//String of who is under it (e.g: On(A,B)
+	int pos;	//Its position on table
 	
+	/**
+	 * Constructor for a block
+	 * @param id Its ID
+	 * @param on String of On action
+	 */
 	Block(char id, String on) {
 		this.id = id;
 		this.on = on;
 	}
 
+	/**
+	 * Get ID
+	 * @return Blocks' ID 
+	 */
 	public char getId() {
 		return id;
 	}
 
+	/**
+	 * Get on
+	 * @return On action's string
+	 */
 	public String getOn() {
 		return on;
 	}
 	
+	/**
+	 * Set on
+	 * @param on String of On action
+	 */
 	public void setOn(String on) {
 		this.on = on;
 	}
 
+	/**
+	 * Get position
+	 * @return Its position
+	 */
 	public int getPos() {
 		return pos;
 	}
--- src/main/java/ia_projeto/ia_projeto/BlocksGoalTest.java
+++ src/main/java/ia_projeto/ia_projeto/BlocksGoalTest.java
@@ -5,14 +5,27 @@ import java.util.Hashtable;
 
 import aima.core.search.framework.GoalTest;
 
+/**
+ * Class has goal world state
+ * @author Andre
+ * @author Daniel
+ * @author Paulo
+ */
 public class BlocksGoalTest implements GoalTest{
 
 	ArrayList<String> functions;
 	
+	/**
+	 * Constructor for class GoalTest
+	 * @param functions List of Actions that describes the world (e.g: On(A,table), Clear(B))
+	 */
 	public BlocksGoalTest(ArrayList<String> functions) {
 		this.functions = functions;
 	}
 	
+	/**
+	 * Return true if the current world is equal to goal world
+	 */
 	public boolean isGoalState(Object state) {
 		State currentState = (State) state;
 		for(String s : currentState.functions()) {
